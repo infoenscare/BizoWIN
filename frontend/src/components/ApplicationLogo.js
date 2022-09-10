9d829094a93fd1eaac712cad0a7e8eb38ccfa443
Pratik Phopare <pratik.phopare@qubesense.com>
2022-09-11 00:10:47 +0530

    First Push to Branch Pratik
    First Push to Branch Pratik

---
+++ frontend/src/components/ApplicationLogo.js
@@ -0,0 +1,17 @@
+const ApplicationLogo = props => (
+<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 483 483">
+  <g id="BizowinSVG" transform="translate(-612 -281)">
+    <circle id="Ellipse_3" data-name="Ellipse 3" cx="241.5" cy="241.5" r="241.5" transform="translate(612 281)" fill="#24172d"/>
+    <path id="Intersection_1" data-name="Intersection 1" d="M57.295,307.166,450.633,61.38a241.5,241.5,0,0,1,71.592,114.454L128.886,421.62A241.5,241.5,0,0,1,57.295,307.166Z" transform="translate(563.74 281)" fill="#ffbd03"/>
+    <circle id="Ellipse_1" data-name="Ellipse 1" cx="219" cy="219" r="219" transform="translate(635 304)" fill="#25172e"/>
+    <g id="Group_1" data-name="Group 1" transform="translate(677.244 464.081)">
+      <text id="WIN" transform="translate(185.756 91.919)" fill="#febe06" font-size="89" font-family="Swiss721BT-BoldItalic, Swis721 BT" font-weight="700" font-style="italic"><tspan x="0" y="0">WIN</tspan></text>
+      <text id="Bizo" transform="translate(-0.244 85.919)" fill="#fff" font-size="89" font-family="Swiss721BT-BoldItalic, Swis721 BT" font-weight="700" font-style="italic"><tspan x="0" y="0">Bizo</tspan></text>
+    </g>
+  </g>
+</svg>
+
+  
+)
+
+export default ApplicationLogo
